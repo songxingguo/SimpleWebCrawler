1bc32c7a45b8a0ad3febb1481632dbd7ef805579
songxingguo <1328989942@qqc.com>
2018-10-24 18:31:38 +0800

    获取代码

--- routes/index.js
+++ routes/index.js
@@ -9,71 +9,42 @@ router.get('/', async (ctx, next) => {
     })
 });
 
-router.get('/getJobs', async (req, res, next) => {
+router.get('/getJobs', async (ctx, next) => {
     var infos = [];
     var url = 'http://tz.cqut.edu.cn/bmtz.htm';
-    http.get(url+page,function(res){  //通过get方法获取对应地址中的页面信息
-        var chunks = [];
-        var size = 0;
-        res.on('data',function(chunk){   //监听事件 传输
-            chunks.push(chunk);
-            size += chunk.length;
-        });
-        res.on('end',function(){  //数据传输完
-            var data = Buffer.concat(chunks,size);
-            var html = data.toString();
-            //    console.log(html);
-            var $ = cheerio.load(html); //cheerio模块开始处理 DOM处理
-            $("#tzlb>li").each(function(){   //对页面岗位栏信息进行处理  每个岗位对应一个 li  ,各标识符到页面进行分析得出
-                var info = {};
-                info.title = $(this).find(".record .title .head").text(); //通知名称
-                console.log(info.title);  //控制台输出岗位名
-                infos.push(info);
-            });
-        });
-    });
 
-    ctx.response.body = infos;
-});
-
-router.get('/json', async (ctx, next) => {
-    //url 获取信息的页面部分地址
-    var url = 'http://tz.cqut.edu.cn/bmtz.htm';
-    var infos = [];
-
-    const promise = function(url){
-        return new Promise((resovle,reject)=>{
+    function promise() {
+        return new Promise(function (resolve, reject) {
             http.get(url,function(res){  //通过get方法获取对应地址中的页面信息
                 var chunks = [];
                 var size = 0;
                 res.on('data',function(chunk){   //监听事件 传输
-
+                    chunks.push(chunk);
+                    size += chunk.length;
                 });
                 res.on('end',function(){  //数据传输完
-                    resovle(chunks)
+                    var data = Buffer.concat(chunks,size);
+                    var html = data.toString();
+                    //    console.log(html);
+                    var $ = cheerio.load(html); //cheerio模块开始处理 DOM处理
+                    $("#tzlb>li").each(function(){   //对页面岗位栏信息进行处理  每个岗位对应一个 li  ,各标识符到页面进行分析得出
+                        var info = {};
+                        info.title = $(this).find(".record .title .head").text(); //通知名称
+                        info.src = "http://tz.cqut.edu.cn/" + $(this).find(".record .title").attr("href"); //通知链接
+                        console.log(info.title);  //控制台输出岗位名
+                        infos.push(info);
+                    });
+                    resolve();
                 });
-            });
+            })
         })
-    };
+    }
 
-    await promise(url).then(function (chunks) {
-        var data = Buffer.concat(chunks,size);
-        var html = data.toString();
-        //    console.log(html);
-        var $ = cheerio.load(html); //cheerio模块开始处理 DOM处理
-        $("#tzlb>li").each(function(){   //对页面岗位栏信息进行处理  每个岗位对应一个 li  ,各标识符到页面进行分析得出
-            var info = {};
-            info.title = $(this).find(".record .title .head").text(); //通知名称
-            console.log(info.title);  //控制台输出岗位名
-            infos.push(info);
-        });
-    }).then(function () {
-        chunks.push(chunk);
-        size += chunk.length;
-    });
+    await promise().then();
 
-    ctx.response.type = 'text/plain';
-    ctx.response.body = infos;
-})
+    ctx.body = {
+        infos: infos
+    }
+});
 
 module.exports = router;
