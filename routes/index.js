2b5b8b3f54ba01fd1c439849925c7804dcb27e5b
songxingguo <1328989942@qqc.com>
2018-10-26 02:06:16 +0800

    https

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', async (ctx, next) => {
     })
 });
 
-router.get('/getJobs', async (ctx, next) => {
+router.get('/getNotices', async (ctx, next) => {
     var infos = [];
     var url = 'http://tz.cqut.edu.cn/bmtz.htm';
 
@@ -44,6 +44,41 @@ router.get('/getJobs', async (ctx, next) => {
 
     ctx.body = {
         infos: infos
+    }
+});
+
+router.post('/getNotice', async (ctx, next) => {
+    console.log(ctx);
+    var url = ctx.request.body.url;
+    var notice = {
+        content: ""
+    };
+    function promise() {
+        return new Promise(function (resolve, reject) {
+            http.get(url,function(res){  //通过get方法获取对应地址中的页面信息
+                var chunks = [];
+                var size = 0;
+                res.on('data',function(chunk){   //监听事件 传输
+                    chunks.push(chunk);
+                    size += chunk.length;
+                });
+                res.on('end',function(){  //数据传输完
+                    var data = Buffer.concat(chunks,size);
+                    var html = data.toString();
+                    var $ = cheerio.load(html); //cheerio模块开始处理 DOM处理
+                    $("#vsb_content p").each(function(){   //对页面岗位栏信息进行处理  每个岗位对应一个 li  ,各标识符到页面进行分析得出
+                        notice.content += $(this).find(".MsoNormal span").text();
+                    });
+                    resolve();
+                });
+            })
+        })
+    }
+
+    await promise().then();
+
+    ctx.body = {
+        notice: notice
     }
 });
 
